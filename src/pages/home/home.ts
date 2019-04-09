61811c662360cf344492a27f05a598285218ff11
George Dobrin <georgedobrin11@gmail.com>
2019-04-09 22:54:06 +0300

    init commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,62 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  latitude: any;
+  longitude: any;
+  timestamp: any;
+  pastLocations = [];
 
+  constructor(public navCtrl: NavController, private backgroundGeolocation: BackgroundGeolocation, public zone: NgZone) {
   }
+
+  startLocationTrack(): void {
+
+    const config: BackgroundGeolocationConfig = {
+      desiredAccuracy: 10,
+      stationaryRadius: 20,
+      distanceFilter: 2,
+      interval: 2000,
+      debug: false, 
+      stopOnTerminate: false
+    };
+    this.backgroundGeolocation.configure(config).subscribe((location) => {
+
+      this.zone.run(() => {
+        this.latitude = location.latitude;
+        this.longitude = location.longitude;
+
+        this.timestamp = new Date();
+
+        const newLoc = {};
+        newLoc['latitude'] = location.latitude;
+        newLoc['longitude'] = location.longitude;
+        newLoc['timestamp'] = new Date();
+
+        this.pastLocations.push(newLoc);
+
+      });
+      console.log('location', location);
+      this.backgroundGeolocation.finish();
+
+    },
+    (err) => {
+      console.warn(err);
+    });
+    
+    this.backgroundGeolocation.start();
+  }
+
+  stopLocationTrack(): void {
+    this.backgroundGeolocation.stop();
+  }
+
+  showLocationsHistory(): void{
+    this.backgroundGeolocation.getLocations().then(res => {
+      console.log('Locations History', res);
+    });
+  }
+
+
+  
 
 }
